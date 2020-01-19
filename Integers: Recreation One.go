838efb399298613bfa1c1a17991006527d285820
Andrey G <androgen87@gmail.com>
2020-01-19 12:50:02 +0300

    Create Integers: Recreation One.go

---
+++ Integers: Recreation One.go
@@ -0,0 +1,22 @@
+package kata
+import (
+	"fmt"
+	"math"
+)
+func ListSquared(m, n int)  [][]int {
+	var  p float64 =0
+	var res [][]int
+  fmt.Println(m,"|",n)
+	for x:=m;x<n+1;x++{
+		p=0
+		for i:=1;i<x+1;i++{
+			if x%i==0{
+				p+=float64(i*i)
+			}
+		}
+if math.Sqrt(p)-math.Round(math.Sqrt(p))==0{
+res=append(res,[]int{x,int(p)})
+}
+	}
+return res
+}
